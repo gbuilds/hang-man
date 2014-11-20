b67d772865364559413e472381b86a39a3ce0c4e
Gordie Smith <agsmith4@gmail.com>
2014-11-20 04:30:43 +0000

    Added save game function, pirate lingo.

--- hangman.rb
+++ hangman.rb
@@ -1,4 +1,4 @@
-require 'yaml'
+require 'pstore'
 
 class Game
   attr_accessor :name, :playing, :lives
@@ -7,8 +7,8 @@ class Game
     @name = name
     @playing = true
     @lives = 9
-    @dictionary = load_dictionary
-    @word = get_random_word(@dictionary)
+    dictionary = load_dictionary
+    @word = get_random_word(dictionary)
     @letters = @word.split("")
     @matched_letters = Array.new(@word.length, "_")
     @guessed = []
@@ -23,29 +23,73 @@ class Game
   
   def get_random_word(dictionary)
     random_word = dictionary[rand(0..dictionary.length - 1)]
-    p random_word
     random_word = random_word.downcase
   end
   
-  def play   
+  def start   
+    puts "Starting Pirate Hangman. All letters are lowercase."
+    puts "Enter 'save' to save and exit the game."
+    puts "Do you want to start from your pre-existing game? Choose no to start new game. y/n"
+    answer = gets.chomp
+    load_game if answer == "y"
+    play
+  end
+  
+  def play
     while @playing == true
       puts "Guessed: " + @guessed.join(", ") unless @guessed.empty?
       puts @matched_letters.join(" ")
       puts "#{lives} lives. What is your guess?"
       guess = gets.chomp
-      @guessed << guess
+      save_game if guess == "save"
+      @guessed << guess unless guess == "save"
       hit = false
       @letters.each_with_index do |letter, index|
         if letter == guess
           @matched_letters[index] = letter
+          puts "ARRRRRR Matey!!!" if letter == "r"
           hit = true
         end
       end
       @lives -= 1 if hit == false
-      @playing = false if @matched_letters == @letters
-      @playing = false if @lives == 0
+      if @matched_letters == @letters
+        @playing = false 
+        puts "Nice job guessing the word. The King grants you a pardon. The crowd goes wild."
+        puts "You live on while dazzling your pirate crew with words like #{@word}."
+      elsif @lives == 0
+        @playing = false 
+        puts "After one final chance you couldn't solve the word, so you were hanged."
+        puts "By the way, it was #{@word}. You'll have great vocabulary in the afterlife!"
+      end
     end
-    puts @word
+
+  end
+  
+  def save_game
+    saved_game = PStore.new("saved_game.pstore")
+    saved_game.transaction do
+      saved_game[:name] = @name
+      saved_game[:playing] = @playing
+      saved_game[:lives] = @lives
+      saved_game[:word] = @word
+      saved_game[:letters] = @letters
+      saved_game[:matched_letters] = @matched_letters
+      saved_game[:guessed] = @guessed
+    end
+    @playing = false
+  end
+  
+  def load_game
+    game = PStore.new("saved_game.pstore")
+    game.transaction do
+      @name = game[:name]
+      @playing = game[:playing]
+      @lives = game[:lives]
+      @word = game[:word]
+      @letters = game[:letters]
+      @matched_letters = game[:matched_letters]
+      @guessed = game[:guessed]
+    end
   end
   
 end
@@ -62,4 +106,5 @@ end
 # when you elect to save game you need to save: name, random_word, guesses, matched_letters, lives_remaining
 
 mygame = Game.new("Gordie")
-mygame.play
+mygame.start
+
